cc4157b40e4b1db5e7c15c9caebb7e27936a7d55
mrw1593 <botahamec@outlook.com>
2022-02-26 22:32:35 -0500

    Stuff

--- Flutter/three_little_things/lib/main.dart
+++ Flutter/three_little_things/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class FirebaseData {
   CollectionReference<Map<String, dynamic>> thoughtCollection;
@@ -11,7 +9,38 @@ class FirebaseData {
   FirebaseData(this.thoughtCollection, this.uuid);
 }
 
-void main() => runApp(const MyApp());
+class DayThoughts {
+  String userId;
+  List<String> tags;
+  
+  DayThoughts(this.userId, this.tags);  
+  
+  DayThoughts.fromJson(Map<String, Object?> json) : this(
+    json['userId']! as String,
+    json['tags']! as List<String>
+  );
+  
+  Map<String, Object?> toJson() {
+    return {
+      'userId': userId,
+      'tags': tags
+    };
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: const FirebaseOptions(
+    apiKey: "AIzaSyCqCZ4hj54UALwigomO-6LKJ4kS8ZxNuAg",
+    authDomain: "three-little-things.firebaseapp.com",
+    projectId: "three-little-things",
+    storageBucket: "three-little-things.appspot.com",
+    messagingSenderId: "312349347082",
+    appId: "1:312349347082:web:32f6eb7768dc2528ba7ef3",
+    measurementId: "G-W5FHGWVEC5"
+  ));
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -33,66 +62,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.lightGreen,
       ),
-      home: const FirstRoute(),
-    );
-  }
-}
-
-class FirstRoute extends StatelessWidget {
-  const FirstRoute({Key? key}) : super(key: key);
-  
-  Future<FirebaseData> getData() async {
-    await Firebase.initializeApp(options: FirebaseOptions(
-      apiKey: "AIzaSyCqCZ4hj54UALwigomO-6LKJ4kS8ZxNuAg",
-      authDomain: "three-little-things.firebaseapp.com",
-      projectId: "three-little-things",
-      storageBucket: "three-little-things.appspot.com",
-      messagingSenderId: "312349347082",
-      appId: "1:312349347082:web:32f6eb7768dc2528ba7ef3",
-      measurementId: "G-W5FHGWVEC5"
-    ));
-    
-    /*
-    var googleUser = await GoogleSignIn().signIn();
-    var googleAuth = await googleUser?.authentication;
-    var credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken
-    );    
-    var userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
-    */
-    
-    var thoughtCollection = FirebaseFirestore.instance.collection("thoughts");
-    
-    return FirebaseData(thoughtCollection, "test");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: getData(),
-        builder: (context, data) {
-          if (data.hasError) {
-            return Stack();
-          }
-
-          if (data.connectionState == ConnectionState.done) {
-            return const AppLayout();
-          }
-
-          return const Loading();
-        });
-  }
-}
-
-class Loading extends StatelessWidget {
-  const Loading({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: const [CircularProgressIndicator()],
+      home: const AppLayout(),
     );
   }
 }
@@ -100,6 +70,7 @@ class Loading extends StatelessWidget {
 enum Page { analytics, addEntry, community, settings }
 
 class AppLayout extends StatefulWidget {
+  
   const AppLayout({Key? key}) : super(key: key);
 
   @override
@@ -192,6 +163,7 @@ class _App extends State<AppLayout> {
 
 // ANALYTICS PAGE
 class AnalyticsPage extends StatefulWidget {
+  
   const AnalyticsPage({Key? key}) : super(key: key);
 
   @override
@@ -202,9 +174,16 @@ enum ChartDisplayDuration { week, month, year, life }
 
 class _AnalyticsPage extends State<AnalyticsPage> {
   ChartDisplayDuration _displayTime = ChartDisplayDuration.week;
+  final Map<String, int> _tagsOccurrences = {};
 
   @override
   Widget build(BuildContext context) {
+    CollectionReference thoughts = FirebaseFirestore.instance.collection("thoughts");
+    var userThoughts = thoughts
+        .where("user_id", isEqualTo: "test")
+        .get()
+        .then((thoughts) => thoughts.);
+    
     return Positioned.fill(
         child: Align(
             alignment: Alignment.topCenter,
@@ -264,30 +243,8 @@ class _AddEntryPage extends State<AddEntryPage> {
               Text("What made you happy today?",
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-              TextField()])
+            ])
         )
     );
   }
 }
-
-// class _AddEntryPage extends State<AnalyticsPage> {
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Positioned.fill(
-//         child: Align(
-//           alignment: Alignment.topCenter,
-//           child: Column(children: [
-//             const Text("What made you happy today?",
-//             textAlign: TextAlign.center,
-//             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-//           TextField(
-//             decoration:null
-//           )
-//           ])
-//
-//     )
-//     }
-//
-//   )
-// }
